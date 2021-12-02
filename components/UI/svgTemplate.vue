8a70d38b1a0ff050485d9225ed71db98b187311e
Nikita Klyuev <nvk-web@ya.ru>
2021-12-02 17:25:05 +0300

    finish create project hierachy

---
+++ components/UI/svgTemplate.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: 'SvgSprite',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    hrefName() {
+      return `#${this.name}`
+    },
+  },
+}
+</script>
+
+<template>
+  <svg class="svg-icon" :class="cssClass">
+    <use :xlink:href="hrefName" width="100%" height="100%" />
+  </svg>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<script>
+import SvgSprites from '@/components/UI/svgSprites.vue'
+export default {
+    components:{
+        SvgSprites
+    }
+}
+</script>
+
+
+<template>
+    <div>
+        <Nuxt />
+        <Svg-sprites/>
+    </div>
+</template>
